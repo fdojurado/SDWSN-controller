d5e5543f2509fd9c0a687c5b77ea23e2e791dd75
Fernando Jurado <ffjla@dtu.dk>
2021-11-17 21:10:44 +0100

    added dockerfile, requirement.txt, setup.py requirements read,

--- controller/setup.py
+++ controller/setup.py
@@ -1,5 +1,5 @@
 from importlib import import_module
-# from pathlib import Path
+from pathlib import Path
 from setuptools import setup, find_packages
 
 SRC_ROOT = 'controller'
@@ -8,13 +8,13 @@ BIN_ROOT = 'bin/'
 about = import_module(SRC_ROOT + '.about')
 
 
-# with Path('README.md').open('r') as fh:
-#     long_description = fh.read()
+with Path('README.md').open('r') as fh:
+    long_description = fh.read()
 
-# with Path('requirements.txt').open('r') as fh:
-#     requirements = fh.read().splitlines()
-#     requirements = [requirement for requirement in requirements
-#                     if not requirement.startswith('#')]
+with Path('requirements.txt').open('r') as fh:
+    requirements = fh.read().splitlines()
+    requirements = [requirement for requirement in requirements
+                    if not requirement.startswith('#')]
 
 binaries = [BIN_ROOT + about.APP]
 
@@ -30,7 +30,7 @@ setup(
         'Tracker': about.TRACKER_URL,
     },
     # packages=['controller'],  # same as name
-    install_requires=['bar','plac'],  # external packages as dependencies
+    install_requires=requirements,
     packages=find_packages('controller'),
     package_dir={'': SRC_ROOT},
     python_requires='>=3',
