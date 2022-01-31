2d4138031438c9710b997de71efa29ebd4819cb6
Fernando Jurado Lasso <fdo.jurado@gmail.com>
2022-01-31 12:27:33 +0100

    number of sensor nodes function in routing added.

--- controller/controller/routing/routing.py
+++ controller/controller/routing/routing.py
@@ -4,7 +4,11 @@ to reconfigure sensor nodes path. """
 
 # from controller.routing.dijkstra.dijkstra import Graph
 
-import time, threading
+import time
+import threading
+import pandas as pd
+from controller.database.database import Database
+
 
 class Routing():
     def __init__(self, config):
@@ -17,7 +21,28 @@ class Routing():
     def compute_routing(self):
         print("Computing routing algorithm")
         """ We need to read the forwarding table for every node in the database """
-        
+        N = self.num_nodes()
+        print("number of sensor nodes")
+        print(N)
+
         """ We first need to check whether the given graph is connected or not """
         # print(time.ctime())
-        threading.Timer(int(self.config.routing.time), self.compute_routing).start()
+        threading.Timer(int(self.config.routing.time),
+                        self.compute_routing).start()
+
+    """ This function returns the number of sensor nodes in the network """
+
+    def num_nodes(self):
+        # Let's check firs if the collection exists
+        db = Database.find_one("links", {})
+        if(db is None):
+            print('no entries yet, alg. computing aborting')
+            return 0
+        else:
+            df = pd.DataFrame(list(Database.find("links", {})))
+            print("df")
+            print(df)
+            # Now we want to count the number of element
+            values = pd.unique(df[['scr', 'dst']].values.ravel('K'))
+            num = len(values)
+            return num
