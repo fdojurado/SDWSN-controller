6b8f042e9c74aa0b30b8bd5004538d5416b92a7d
Fernando Jurado Lasso <fdo.jurado@gmail.com>
2022-05-09 16:36:48 +0200

    update agent with double q learning

--- controller/controller/deep_reinforcement_learning/agent.py
+++ controller/controller/deep_reinforcement_learning/agent.py
@@ -39,16 +39,18 @@ class Agent:
 
     def store(self, state, action, reward, next_state, done):
         self.experience_replay.append(
-            (np.expand_dims(state, axis=0), action, reward, np.expand_dims(next_state, axis=0), done))
+            (state, action, reward, next_state, done))
 
     def _build_compile_model(self):
-        init = tf.keras.initializers.HeUniform()
+        # init = tf.keras.initializers.HeUniform()
         model = Sequential()
-        model.add(Dense(24, input_shape=self._state_size, activation='relu', kernel_initializer=init))
+        model.add(Dense(32, input_shape=(4,), activation='relu'))
+        # model.add(Dense(32, input_shape=self._state_size,
+        #           activation='relu', kernel_initializer=init))
         # model.add(Embedding(self._state_size, 10, input_length=1))
         # model.add(Reshape((10,)))
         model.add(Dense(32, activation='relu'))
-        model.add(Dense(32, activation='relu'))
+        # model.add(Dense(2, activation='relu'))
         model.add(Dense(self._action_size, activation='linear'))
 
         model.compile(loss='mse', optimizer=self._optimizer)
@@ -57,36 +59,56 @@ class Agent:
     def align_target_model(self):
         self.target_network.set_weights(self.q_network.get_weights())
 
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return self.env.action_space.sample()
+    def save_exp_model(self):
+        np.save('memory', self.experience_replay)
+        self.q_network.save('tmp_model')
 
-        q_values = self.q_network.predict(np.expand_dims(state, axis=0))
-        return np.argmax(q_values[0])
+    def epsilon_greedy(self, state):
+        # exploration
+        if np.random.random() < self.epsilon:
+            # exploration
+            action = np.random.randint(0, self.env.action_space.n)
+            return action
+        else:
+            # exploitation
+            # use expand_dims here to add a dimension for input layer
+            q_vals = self.q_network.predict(state)
+            action = np.argmax(q_vals)
+            return action
 
     def retrain(self, batch_size):
+        # if memory is less than batch size, return nothing
         if len(self.experience_replay) < batch_size:
             return
 
+        # sample a batch
         minibatch = random.sample(self.experience_replay, batch_size)
 
-        for state, action, reward, next_state, done in minibatch:
+        # iterate through batch
+        for state, action, reward, new_state, done in minibatch:
+            # scale states to be [0,1]. We only scale before fitting cuz storing uint8 is cheaper
+            # state = state/255
+            # new_state = new_state/255
 
-            if done:
-                max_future_q = reward
-            else:
-                # what we think the state's q_val should be, reward + discounted future utility
-                max_future_q = reward + self.discount_factor * \
-                    np.max(self.q_network.predict(next_state))
+            target = reward
 
-            # what we thought the current state's q_val should be
-            target_vector = self.target_network.predict(next_state)[0]
+            # if game not over, target q val includes discounted future utility
+            # we use a cloned model to predict here for stability. Model is changed every C frames
+            # we use the online model to choose best action to deal with overestimation error (Double-Q learning)
+            if not done:
+                best_future_action = np.argmax(
+                    self.q_network.predict(new_state))
 
-            # update the target_vector
-            target_vector[action] = max_future_q
+                target = reward + self.discount_factor * \
+                    self.target_network.predict(
+                        new_state)[0][best_future_action]
 
-            # instead of finding temporal difference between new q_val and old q_val, we train the model by giving it the new q_val
-            # and let the network do the updating
+            # get current actions vector
+            target_vector = self.q_network.predict(state)[0]
+
+            # update current action q val with target q val
+            target_vector[action] = target
+            # fit model
             # train the model with the batch
             self.q_network.fit(
                 state, target_vector.reshape(-1, self.env.action_space.n), epochs=1, verbose=0)
--- controller/controller/deep_reinforcement_learning/drl.py
+++ controller/controller/deep_reinforcement_learning/drl.py
@@ -1,8 +1,10 @@
 # from controller.deep_reinforcement_learning.agent import Agent
 from agent import Agent
 import tensorflow as tf
+from tqdm import tqdm
 from tensorflow.keras.optimizers import Adam
 import gym
+import numpy as np
 
 DISABLE_GPU = True
 
@@ -31,48 +33,64 @@ max_epsilon = 0.5  # You can't explore more than 100% of the time
 min_epsilon = 0.01  # At a minimum, we'll always explore 1% of the time
 decay = 0.01
 
-optimizer = Adam(learning_rate=0.01)
+# Frequency of target update
+
+TARGET_UPDATE = 100
+
+optimizer = Adam()
 agent = Agent(env, optimizer, max_epsilon, min_epsilon, decay)
 
 batch_size = 32
-num_of_episodes = 100
+num_episodes = 50
 agent.q_network.summary()
 
-steps_to_update_target_model = 0
+# init num_updates
+num_updates = 0
 
-for episode in range(num_of_episodes):
+for episode in tqdm(range(num_episodes), position=0, leave=True):
     total_training_rewards = 0
     state = env.reset()
-    reward = 0
     done = False
 
     while not done:
-        steps_to_update_target_model += 1
+
         if False:
             env.render()
+
         # Run Action
-        action = agent.act(state)
+        action = agent.epsilon_greedy(np.expand_dims(state, axis=0))
 
         # Take action
         next_state, reward, done, _ = env.step(action)
-        agent.store(state, action, reward, next_state, done)
+        # modify reward so it scales with pole angle. Pole angle range [-0.418, 0.418]
+        reward = 1 - abs(state[2])/0.418
 
-        state = next_state
+        # memorize
+        agent.store(np.expand_dims(state, axis=0), action, reward,
+                    np.expand_dims(next_state, axis=0), done)
+
+        # Experience replay
+        agent.retrain(batch_size)
+
+        # clone target network every C frames
+        num_updates += 1
+
+        if num_updates >= TARGET_UPDATE:
+            num_updates = 0
+            agent.align_target_model()
+            # save memory and model
+            agent.save_exp_model()
+
         total_training_rewards += reward
-
-        if steps_to_update_target_model % 4 == 0 or done:
-            agent.retrain(batch_size)
 
         if done:
             print('Total training rewards: {} after n steps = {} with final reward = {}'.format(
                 total_training_rewards, episode, reward))
-            total_training_rewards += 1
 
-            if steps_to_update_target_model >= 100:
-                print('Copying main network weights to the target network weights')
-                agent.align_target_model()
-                steps_to_update_target_model = 0
-            break
+        # update state
+        state = next_state
 
+    # decay epsilon
     agent.update_epsilon(episode)
-env.close()
+
+# env.close()
