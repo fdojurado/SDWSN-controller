f835f442f6b60cfbe550a9507632486daeeeacf3
Fernando Jurado <ffjla@dtu.dk>
2022-08-02 16:14:34 +0200

    FIT IoT LAB controller updated with main functionalities.

--- sdwsn_controller/controller/fit_iotlab_controller.py
+++ sdwsn_controller/controller/fit_iotlab_controller.py
@@ -1,29 +1,37 @@
-class FitIoTLABController(BaseController):
-    def __init__(
-            self,
-            socket_host: str = '127.0.0.1',
-            socket_port: int = 60001,
-            db_name: str = 'mySDN',
-            db_host: str = '127.0.0.1',
-            db_port: int = 27017,
-            simulation_name: str = 'mySimulation',
-            processing_window: int = 200,
-            max_channel_offsets: int = 3,
-            max_slotframe_size: int = 500,
-            tsch_scheduler: Optional[str] = 'Contention Free',
-            log_dir: str = "./monitor/"):
-        super().__init__(
-            socket_host,
-            socket_port,
-            db_name,
-            db_host,
-            db_port,
-            simulation_name,
-            processing_window,
-            max_channel_offsets,
-            max_slotframe_size,
-            tsch_scheduler,
-            log_dir)
+from sdwsn_controller.controller.common_controller import CommonController
+from sdwsn_controller.tsch.contention_free_scheduler import ContentionFreeScheduler
 
-    def start(self):
-        print('starting FIT IoT LAB')
+
+class FitIoTLABController(CommonController):
+    def __init__(
+        self,
+        socket_port: int = 2001,
+        db_name: str = 'mySDN',
+        db_host: str = '127.0.0.1',
+        db_port: int = 27017,
+        processing_window: int = 200,
+        tsch_scheduler: object = ContentionFreeScheduler(500, 3)
+    ):
+        super().__init__(
+            port=socket_port,
+            db_name=db_name,
+            db_host=db_host,
+            db_port=db_port,
+            processing_window=processing_window,
+            tsch_scheduler=tsch_scheduler,
+        )
+
+    def fit_iot_lab_start(self):
+        print('starting FIT IoT LAB controller')
+        # Initialize main controller
+        self.start()
+
+    def fit_iot_lab_stop(self):
+        print('starting FIT IoT LAB controller')
+        # Stop main controller
+        self.stop()
+
+    def fit_iot_lab_reset(self):
+        print('Resetting FIT IoT LAB controller')
+        self.fit_iot_lab_stop()
+        self.fit_iot_lab_start()
