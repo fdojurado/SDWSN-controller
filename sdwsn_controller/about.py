f1909e3718a2813f3d9756ce4e7746b76ea169d7
Fernando Jurado <ffjla@dtu.dk>
2022-11-11 19:23:36 +0100

    pyproject update, removed setup,

--- sdwsn_controller/about.py
+++ sdwsn_controller/about.py
@@ -1,35 +1,35 @@
-#!/usr/bin/python3
-#
-# Copyright (C) 2022  Fernando Jurado-Lasso <ffjla@dtu.dk>
+# #!/usr/bin/python3
+# #
+# # Copyright (C) 2022  Fernando Jurado-Lasso <ffjla@dtu.dk>
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# # This program is free software: you can redistribute it and/or modify
+# # it under the terms of the GNU General Public License as published by
+# # the Free Software Foundation, either version 3 of the License, or
+# # (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# # This program is distributed in the hope that it will be useful,
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# # GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# # You should have received a copy of the GNU General Public License
+# # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""This module specifies some information about the package."""
-__project_name__ = 'sdwsn_controller'
-__version__ = '1.0'
-__description__ = 'An open source implementation of the SDWSN controller \
-    for the ELISE project (https://github.com/fdojurado/ELISE).'
-__keywords__ = 'SDWSN controller SDN WSN'
-__author__ = 'Fernando Jurado-Lasso'
-__email__ = 'ffjla@dtu.dk'
-__license__ = 'MIT License'
-__github_url__ = 'https://github.com/fdojurado/SDWSN-controller'
-__doc_url__ = 'https://github.com/fdojurado/SDWSN-controller/wiki'
-__tracker_url__ = 'https://github.com/fdojurado/SDWSN-controller/issues'
-__copyright__ = 'Copyright (c) 2022, Fernando Jurado-Lasso.\nThis program comes with ABSOLUTELY NO WARRANTY.\
- This is free software, and you are welcome to redistribute it under certain conditions.'
-__info_for_scripts__ = 'About: ' + __description__ + "\n" \
-    'Version: ' + __version__ + "\n" \
-    'Author: ' + __author__ + "\n" \
-    ""+__copyright__ + "\n"
+# """This module specifies some information about the package."""
+# __project_name__ = 'sdwsn_controller'
+# __version__ = '1.0'
+# __description__ = 'An open source implementation of the SDWSN controller \
+#     for the ELISE project (https://github.com/fdojurado/ELISE).'
+# __keywords__ = 'SDWSN controller SDN WSN'
+# __author__ = 'Fernando Jurado-Lasso'
+# __email__ = 'ffjla@dtu.dk'
+# __license__ = 'MIT License'
+# __github_url__ = 'https://github.com/fdojurado/SDWSN-controller'
+# __doc_url__ = 'https://github.com/fdojurado/SDWSN-controller/wiki'
+# __tracker_url__ = 'https://github.com/fdojurado/SDWSN-controller/issues'
+# __copyright__ = 'Copyright (c) 2022, Fernando Jurado-Lasso.\nThis program comes with ABSOLUTELY NO WARRANTY.\
+#  This is free software, and you are welcome to redistribute it under certain conditions.'
+# __info_for_scripts__ = 'About: ' + __description__ + "\n" \
+#     'Version: ' + __version__ + "\n" \
+#     'Author: ' + __author__ + "\n" \
+#     ""+__copyright__ + "\n"
