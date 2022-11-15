073939eb00a17b748e78d9ff10c8a47565004d45
Fernando Jurado <ffjla@dtu.dk>
2022-11-15 09:44:25 +0100

    tests updated

--- tests/01-native-controller/test_native_controller.py
+++ tests/01-native-controller/test_native_controller.py
@@ -1,7 +1,14 @@
+import os
+
+
 # content of test_sample.py
 def inc(x):
     return x + 1
 
 
 def test_answer():
+    if os.getenv('CONTIKI_NG'):
+        print('Env exists!')
+    else:
+        print("Env doesn't exist!")
     assert inc(3) == 5
