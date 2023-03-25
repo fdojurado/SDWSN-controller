d61da2139e3c0b6917a6e374c809f32f5e914381
Fernando Jurado <ffjla@dtu.dk>
2023-03-25 09:26:38 +0100

    Reduce overlapping functions in sink.

--- sdwsn_controller/sink_communication/sink_abc.py
+++ sdwsn_controller/sink_communication/sink_abc.py
@@ -31,9 +31,134 @@ class SinkABC(ABC):
     @abstractmethod
     def __init__(
         self,
-        **kwargs: object
     ):
+        """
+        Abstract class for sink communication.
+        """
+        self.byte_msg = bytearray()
+        self.overflow = 0
+        self.escape_character = 0
+        self.frame_start = 0
+        self.frame_length = 0
+        self.msg = None
+        self.ser = None
+
+    @abstractmethod
+    def name(self):
+        """
+        Get the name of the sink.
+
+        Returns:
+            str: Name of the sink.
+        """
         pass
+
+    @abstractmethod
+    def connect(self, **kwargs):
+        """
+        Connect to the sink.
+        """
+        pass
+
+    @abstractmethod
+    def read_byte(self):
+        """
+        Read a byte from the sink.
+
+        Returns:
+            int: Byte read.
+        """
+        pass
+
+    @abstractmethod
+    def error_exception(self):
+        """
+        Check if there is an error in the sink.
+
+        Returns:
+            int: Error code.
+        """
+        pass
+
+    @abstractmethod
+    def send_stream_bytes(self, stream_bytes):
+        """
+        Send a stream of bytes to the sink.
+
+        Args:
+            stream_bytes (bytes): Bytes to send.
+        """
+        pass
+
+    def _recv_internal(self, timeout):
+        """
+        Read a message from the sink.
+
+        Args:
+            timeout (_type_):  Seconds to wait for a message.
+
+        Returns:
+            int, Message: Message received.
+        """
+        try:
+            # ser.read can return an empty string
+            # or raise a SerialException
+            rx_byte = self.read_byte()
+            # logger.info("rx_byte")
+            # logger.info(rx_byte)
+            if rx_byte and ord(rx_byte) != 0x7E:
+                if (self.escape_character):
+                    self.escape_character = 0
+                    a = int.from_bytes(rx_byte, 'big')
+                    b = int.from_bytes(b'\x20', 'big')
+                    rx_byte = a ^ b
+                    rx_byte = rx_byte.to_bytes(1, 'big')
+                elif (rx_byte and ord(rx_byte) == 0x7D):
+                    self.escape_character = 1
+                    return 0
+
+                if (self.frame_length < (122 + 2)):
+                    # Adding 2 bytes from serial communication
+                    self.byte_msg.extend(rx_byte)
+                    self.frame_length = self.frame_length + 1
+                else:
+                    self.overflow = 1
+                    # logger.info("Packet size overflow: %u bytes\n",
+                    #   self.frame_length)
+                    return 0
+            else:
+                # logger.info("FRAME_BOUNDARY_OCTET detected")
+                if (self.escape_character == 1):
+                    # logger.info("serial: escape_character == true")
+                    self.escape_character = 0
+                elif (self.overflow):
+                    # logger.info("serial overflow")
+                    self.overflow = 0
+                    self.frame_length = 0
+                    self.frame_start = 1
+                    self.byte_msg = bytearray()
+                elif (self.frame_length >= 6 and self.frame_start):
+                    # Wake up consumer process
+                    # logger.info("Wake up consumer process")
+                    self.frame_start = 0
+                    self.overflow = 0
+                    self.frame_length = 0
+                    msg_buffer = self.byte_msg
+                    self.byte_msg = bytearray()
+                    return msg_buffer, False
+                else:
+                    # re-synchronization. Start over
+                    # logger.info("serial re-synchronization\n")
+                    self.frame_start = 1
+                    self.byte_msg = bytearray()
+                    self.frame_length = 0
+                    return 0
+                return 0
+
+        except self.error_exception():
+            return None
+
+        return 0
 
     def recv(self, timeout: Optional[float] = None):
         """
@@ -66,35 +191,50 @@ class SinkABC(ABC):
                 else:
                     return None
 
-    @abstractmethod
-    def _recv_internal(
-        self, timeout: Optional[float]
-    ):
-        """
-        Read a message from the sink.
+    def check_byte(self, byte_data, data):
+        if (ord(data) == 0x7E or ord(data) == 0x7D):
+            byte_data.extend(bytes.fromhex('7D'))
+            invert = ord(data) ^ ord(b'\x20')
+            invert_bytes = invert.to_bytes(len(data), sys.byteorder)
+            byte_data.extend(invert_bytes)
+        else:
+            byte_data.extend(data)
 
-        Args:
-            timeout (Optional[float]): Seconds to wait for a message.
-
-        Raises:
-            NotImplementedError: Not implemented error.
-        """
-        raise NotImplementedError("Trying to read from a write only bus?")
-
-    @abstractmethod
-    def send(self, msg, timeout: Optional[float] = None) -> None:
+    def send(self, data) -> None:
         """
         Transmit a message to the sink
 
         Args:
-            msg (Message): Message object to transmit.
-            timeout (Optional[float], optional): Wait for up to n seconds
-                to the message be acked. Defaults to None.
-
-        Raises:
-            NotImplementedError: _description_
+            data (Message): Message object to transmit.
         """
-        raise NotImplementedError("Trying to write to a readonly bus?")
+        logger.debug('Sending message over the serial interface')
+        byte_msg = bytearray()
+        byte_msg.extend(bytes.fromhex('7E'))
+        data = [data[i:i+1] for i in range(len(data))]
+        for i in range(0, len(data)):
+            # logger.info('data')
+            # logger.info((data[i]))
+            self.check_byte(byte_msg, data[i])
+        byte_msg.extend(bytes.fromhex('7E'))
+        logger.debug('packet to send')
+        logger.debug(byte_msg.hex())
+        self.send_stream_bytes(byte_msg)
+
+    def empty_socket(self):
+        try:
+            while self.recv(0.1):
+                pass
+        except TypeError:
+            pass
+
+    def shutdown(self) -> None:
+        """
+        Close the serial interface.
+        """
+        if self.ser is not None:
+            self.empty_socket()
+            logger.debug("socket buffer is now empty, we close ...")
+            self.ser.close()
 
     def __iter__(self):
         """Allow iteration on messages as they are received.
--- sdwsn_controller/sink_communication/sink_serial.py
+++ sdwsn_controller/sink_communication/sink_serial.py
@@ -33,142 +33,20 @@ class SinkSerial(SinkABC):
         assert isinstance(config.sink_comm.port_baud, int)
         self.dev = config.sink_comm.host_dev
         self.baud = config.sink_comm.port_baud
-        self.byte_msg = bytearray()
-        self.overflow = 0
-        self.escape_character = 0
-        self.frame_start = 0
-        self.frame_length = 0
-        self.msg = None
-        self.ser = None
-        self.name = "Serial"
+        self.__name = "Serial"
+
+    def name(self):
+        return self.__name
 
     def connect(self, **kwargs):
         self.ser = serial.Serial(self.dev, self.baud)  # open serial port
         return 0
 
-    def _recv_internal(self, timeout):
-        """
-        Read a message from the sink.
+    def read_byte(self):
+        return self.ser.read()
 
-        Args:
-            timeout (_type_):  Seconds to wait for a message.
+    def error_exception(self):
+        return serial.SerialException
 
-        Returns:
-            int, Message: Message received.
-        """
-        try:
-            # ser.read can return an empty string
-            # or raise a SerialException
-            rx_byte = self.ser.read()
-            # logger.info("rx_byte")
-            # logger.info(rx_byte)
-            if rx_byte and ord(rx_byte) != 0x7E:
-                if (self.escape_character):
-                    self.escape_character = 0
-                    a = int.from_bytes(rx_byte, 'big')
-                    b = int.from_bytes(b'\x20', 'big')
-                    rx_byte = a ^ b
-                    rx_byte = rx_byte.to_bytes(1, 'big')
-                elif (rx_byte and ord(rx_byte) == 0x7D):
-                    self.escape_character = 1
-                    return 0
-
-                if (self.frame_length < (122 + 2)):
-                    # Adding 2 bytes from serial communication
-                    self.byte_msg.extend(rx_byte)
-                    self.frame_length = self.frame_length + 1
-                else:
-                    self.overflow = 1
-                    # logger.info("Packet size overflow: %u bytes\n",
-                    #   self.frame_length)
-                    return 0
-            else:
-                # logger.info("FRAME_BOUNDARY_OCTET detected")
-                if (self.escape_character == 1):
-                    # logger.info("serial: escape_character == true")
-                    self.escape_character = 0
-                elif (self.overflow):
-                    # logger.info("serial overflow")
-                    self.overflow = 0
-                    self.frame_length = 0
-                    self.frame_start = 1
-                    self.byte_msg = bytearray()
-                elif (self.frame_length >= 6 and self.frame_start):
-                    # Wake up consumer process
-                    # logger.info("Wake up consumer process")
-                    self.frame_start = 0
-                    self.overflow = 0
-                    self.frame_length = 0
-                    msg_buffer = self.byte_msg
-                    self.byte_msg = bytearray()
-                    return msg_buffer, False
-                else:
-                    # re-synchronization. Start over
-                    # logger.info("serial re-synchronization\n")
-                    self.frame_start = 1
-                    self.byte_msg = bytearray()
-                    self.frame_length = 0
-                    return 0
-                return 0
-
-        except serial.error:
-            return None
-
-        return 0
-
-    def send(self, data):
-        """
-        Transmit a message to the sink
-
-        Args:
-            data (Message): Message object to transmit.
-        """
-        logger.debug('Sending message over the serial interface')
-        byte_msg = bytearray()
-        byte_msg.extend(bytes.fromhex('7E'))
-        data = [data[i:i+1] for i in range(len(data))]
-        for i in range(0, len(data)):
-            # logger.info('data')
-            # logger.info((data[i]))
-            self.check_byte(byte_msg, data[i])
-        byte_msg.extend(bytes.fromhex('7E'))
-        logger.debug('packet to send')
-        logger.debug(byte_msg.hex())
-        self.ser.write(byte_msg)
-
-    def check_byte(self, byte_data, data):
-        if (ord(data) == 0x7E or ord(data) == 0x7D):
-            byte_data.extend(bytes.fromhex('7D'))
-            invert = ord(data) ^ ord(b'\x20')
-            invert_bytes = invert.to_bytes(len(data), sys.byteorder)
-            byte_data.extend(invert_bytes)
-        else:
-            byte_data.extend(data)
-
-    def empty_socket(self):
-        try:
-            while self.recv(0.1):
-                pass
-        except TypeError:
-            pass
-
-    def shutdown(self) -> None:
-        """
-        Close the serial interface.
-        """
-        if self.ser is not None:
-            self.empty_socket()
-            logger.debug("socket buffer is now empty, we close ...")
-            self.ser.close()
-            # self.ser.shutdown(socket.SHUT_RDWR)
-
-    # def read(self):
-    #     while(1):
-    #         try:
-    #             msg = self.recv(0.1)
-    #             if(len(msg) > 0):
-    #                 # send it back to main
-    #                 self.msg = msg
-    #                 # self.output_queue.put(msg)
-    #         except TypeError:
-    #             pass
+    def send_stream_bytes(self, stream_bytes):
+        self.ser.write(stream_bytes)
