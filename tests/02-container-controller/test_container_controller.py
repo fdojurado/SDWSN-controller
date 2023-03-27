c02f1685fe5ccb58ad2a8ad10c24cc901b82cb4a
Fernando Jurado <ffjla@dtu.dk>
2023-03-27 17:29:44 +0200

    Github actions timeout set to 45. Update test folder parameters.

--- tests/02-container-controller/test_container_controller.py
+++ tests/02-container-controller/test_container_controller.py
@@ -70,6 +70,7 @@ def test_container_controller():
     config = SDWSNControllerConfig.from_json_file(CONFIG_FILE)
     config.docker.contiki = os.getenv('CONTIKI_NG')
     config.docker.image = os.getenv('DOCKER_BASE_IMG')
+    config.docker.port = PORT
     config.sink_comm.port_baud = PORT
     controller_class = CONTROLLERS[config.controller_type]
     controller = controller_class(config)
